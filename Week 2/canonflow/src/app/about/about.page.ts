a85770ce1e763c46e2071249307c8f059c63d8df
canonflow <nathangarzyasantoso7@gmail.com>
2025-02-26 15:13:04 +0700

    [Week 2] End

---
+++ Week 2/canonflow/src/app/about/about.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+// import { IonContent, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+  standalone: false,
+  // imports: [IonContent, IonItem, IonLabel, IonList]
+})
+export class AboutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Week 2/canonflow/src/app/app-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'setting',
+    loadChildren: () => import('./setting/setting.module').then( m => m.SettingPageModule)
+  },
+  {
+    path: 'tabs', 
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'list',
+        loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+      },
+      {
+        path: 'setting',
+        loadChildren: () => import('./setting/setting.module').then(m => m.SettingPageModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
